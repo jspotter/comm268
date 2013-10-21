7352d5ca8a59584d578fc3bd68843be784403ac8
Jonathan Potter <potter.jonathan.s@gmail.com>
2013-10-21 10:11:09 -0700

    Added stats.

--- stats/index.php
+++ stats/index.php
@@ -1,12 +1,35 @@
 <?php
-	include("sqlconnect.php");
-
-	include("sqldisconnect.php");
+	include_once("../dbfuncs.php");
+	include("./sqlconnect.php");
+	$result = executeQuery($db, "select * from Data;");
+	include("../sqldisconnect.php");
 ?>
 
 <html>
 	<head>
 	</head>
 	<body>
+		<table border="1px solid black">
+			<tr>
+				<th>SUNet ID</th>
+				<th>Puzzle</th>
+				<th>Seconds Taken</th>
+				<th>Clicks</th>
+				<th>Correct</th>
+			</tr>
+			<?php
+				foreach ($result as $row) {
+			?>
+					<tr>
+						<td><?= $row["sunetid"] ?></td>
+						<td><?= $row["puzzle"] ?></td>
+						<td><?= $row["time"] ?></td>
+						<td><?= $row["clicks"] ?></td>
+						<td><?= $row["correct"] ?></td>
+					</tr>
+			<?php
+				}
+			?>
+		</table>
 	</body>
 </html>
